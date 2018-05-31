18d133faa4a8758b633daba4eae905ca6adec771
Dmitriy <Dmitriy@DESKTOP-CIOUO03>
2018-05-31 12:43:09 +0300

    m

---
+++ cmake-build-debug/CMakeFiles/KS2.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Dmitriy/source/repos/KS2/Client/Client.cpp" "C:/Users/Dmitriy/source/repos/KS2/cmake-build-debug/CMakeFiles/KS2.dir/Client/Client.cpp.obj"
+  "C:/Users/Dmitriy/source/repos/KS2/KS2/Server.cpp" "C:/Users/Dmitriy/source/repos/KS2/cmake-build-debug/CMakeFiles/KS2.dir/KS2/Server.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Client"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/KS2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KS2.dir/Client/Client.cpp.obj"
+  "CMakeFiles/KS2.dir/KS2/Server.cpp.obj"
+  "KS2.pdb"
+  "KS2.exe"
+  "KS2.exe.manifest"
+  "KS2.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KS2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
